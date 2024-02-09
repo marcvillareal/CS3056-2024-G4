4fa8b10500ffd7c0094b0ce2452477392b4ca64b
marcvillareal <105477620+marcvillareal@users.noreply.github.com>
2024-02-09 16:50:14 +0000

    TODO1 - UserTest cases to verify other getter methods

---
+++ Lab1/src/utils/TestUtils.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class TestUtils {
+
+    public static final String TEXT_COLOR_RED = "\033[0;31m";     // RED - FAIL
+    public static final String TEXT_COLOR_GREEN = "\033[0;32m";   // GREEN - PASS
+    public static final String TEXT_COLOR_RESET = "\033[0;30m";   // BLACK - RESET
+
+}
