856acc9294ebf75b91cd2941eabb5d12a7691719
marcvillareal <marcvillareal30@gmail.com>
2024-03-04 01:50:43 +0000

    Added test data setup

--- Lab2/src/org/jfree/data/test/DataUtilitiesTest.java
+++ Lab2/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -1,45 +1,73 @@
 package org.jfree.data.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
 import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.KeyedValues;
 import org.jfree.data.Values2D;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class DataUtilitiesTest {
-	private Values2D values2D;
+
+	private Values2D data;
+	private double[] dataArray;
+	private double[][] dataArray2D;
+	private KeyedValues dataValues;
 
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
+
+		// Setting up for testCalculateColumnTotal and testCalculateRowTotal
 		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
-		values2D = testValues;
-		testValues.addValue(1, 0, 0);
-		testValues.addValue(4, 1, 0);
+
+		testValues.addValue(3, 4, 5);
+		testValues.addValue(3, 0, 11);
+		testValues.addValue(2, 1, 3);
+		testValues.addValue(2, 2, 7);
+
+		data = testValues;
+
+		// Setting up for testCreateNumberArray
+		dataArray = new double[5];
+		dataArray[0] = 1.5;
+		dataArray[1] = 2.5;
+		dataArray[2] = 3.5;
+		dataArray[3] = 4.5;
+		dataArray[4] = 5.5;
+
+		// Setting up for testCreateNumberArray2D
+		dataArray2D = new double[3][4];
+		dataArray2D[0][0] = 1.5;
+		dataArray2D[0][1] = 2.5;
+		dataArray2D[0][2] = 3.5;
+		dataArray2D[0][3] = 4.5;
+		dataArray2D[1][0] = 5.5;
+		dataArray2D[1][1] = 6.5;
+		dataArray2D[1][2] = 7.5;
+		dataArray2D[1][3] = 8.5;
+		dataArray2D[2][0] = 9.5;
+		dataArray2D[2][1] = 10.5;
+		dataArray2D[2][2] = 11.5;
+		dataArray2D[2][3] = 12.5;
+
+		// Setting up for testGetCumulativePercentages
+		DefaultKeyedValues testKeyValues = new DefaultKeyedValues();
+
+		testKeyValues.addValue((Comparable<?>) 0, 5);
+		testKeyValues.addValue((Comparable<?>) 1, 9);
+		testKeyValues.addValue((Comparable<?>) 2, 2);
+		testKeyValues.addValue((Comparable<?>) 3, 4);
+
+		dataValues = DataUtilities.getCumulativePercentages((KeyedValues) testKeyValues);
 	}
 
 	@After
-	public void tearDown() {
-		values2D = null;
-	}
-
-	@Test
-	public void testValidDataAndColumnColumnTotal() {
-		assertEquals("Wrong sum returned. It should be 5.0", 5.0, DataUtilities.calculateColumnTotal(values2D, 0),
-				0.0000001d);
-	}
-
-	@Test
-	public void testNullDataColumnTotal() {
-		try {
-			DataUtilities.calculateColumnTotal(null, 0);
-			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
-		} catch (Exception e) {
-			assertTrue("Incorrect exception type thrown", e.getClass().equals(IllegalArgumentException.class));
-		}
+	public void tearDown() throws Exception {
+		data = null;
+		dataArray = null;
+		dataValues = null;
 	}
 }
